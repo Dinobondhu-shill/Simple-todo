6ee06bbc04f16e2f45721ce41944a58cc6d11049
Dinobondhu-shill <dinu.webdev@gmail.com>
2025-11-30 02:33:34 +0600

    connected neon db and create user table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
 import express, { Request, Response } from 'express';
+import {Pool} from "pg"
 const app = express();
 const port = 5000;
-
 app.use(express.json());
 
+
+// DB
+const pool = new Pool({
+    connectionString: 'postgresql://neondb_owner:npg_qRJ6rPUcYy1C@ep-twilight-wave-a4j6h31q-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
+
+const initDB = async()=>{
+    pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email  VARCHAR(100) NOT NULL UNIQUE,
+            age INT,
+            phone VARCHAR(15) UNIQUE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+        `)
+}
+
+initDB();
+
+
 app.get('/', (req :Request, res : Response)=> {
-    res.send("Hello from next level developer!")
+    res.send("Hello!")
 })
 
 app.post('/', (req:Request, res : Response)=>{
